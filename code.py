061b536afe63027fec21c59c6f21647853293f8a
mike g <goehlemichael@hotmail.com>
2023-11-24 14:55:35 -0500

    more readable

--- code.py
+++ code.py
@@ -38,16 +38,19 @@ pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
 
-def send_co2_data(co2_data_point, feed_location):
+def send_sensor_data(sensor_data_point, feed_location):
     payload = {
-        "value": co2_data_point
+        "value": sensor_data_point
     }
     headers = {
         "Content-Type": "application/json",
         "X-AIO-Key": os.getenv("AIO_KEY")
     }
     try:
-        response = requests.post(feed_location, data=json.dumps(payload), headers=headers)
+        response = requests.post(feed_location,
+                                 data=json.dumps(payload),
+                                 headers=headers
+                                 )
         if response.status_code == 200:
             print("CO2 data sent successfully! {}".format(response.content))
         else:
@@ -55,7 +58,9 @@ def send_co2_data(co2_data_point, feed_location):
     except wifi.WiFiError as error:
         print("WiFi error occurred:", str(error))
         print("Reconnecting to WiFi...")
-        wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD"))
+        wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"),
+                           os.getenv("CIRCUITPY_WIFI_PASSWORD")
+                           )
         print("Connected to WiFi.")
         print(f"My IP address: {wifi.radio.ipv4_address}")
     except adafruit_requests.RequestError as error:
@@ -72,9 +77,15 @@ while True:
             temp_data = scd4x.temperature
             rel_hum_data = scd4x.relative_humidity
 
-            send_co2_data(co2_data, CO2_FEED_URL)
-            send_co2_data(temp_data, TEMP_FEED_URL)
-            send_co2_data(rel_hum_data, REL_HUM_FEED_URL)
+            send_sensor_data(co2_data,
+                             CO2_FEED_URL
+                             )
+            send_sensor_data(temp_data,
+                             TEMP_FEED_URL
+                             )
+            send_sensor_data(rel_hum_data,
+                             REL_HUM_FEED_URL
+                             )
 
             time.sleep(SEND_DATA_PERIOD)
     except Exception as e:
